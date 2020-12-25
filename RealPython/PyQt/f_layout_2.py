228f9c7f50a82f7a3c82f3ff6a30285fe7db7d70
Ricky Mitchell <ramitch91@gmail.com>
2020-12-25 01:52:24 -0600

    add vertical spacing to f_layout_2.py to keep the spacing consistent

--- RealPython/PyQt/f_layout_2.py
+++ RealPython/PyQt/f_layout_2.py
@@ -14,13 +14,20 @@ class Window(QWidget):
         super(Window, self).__init__()
         self.setWindowTitle("QFormLayout Example")
         self.resize(270, 110)
+
         # Create a QFormLayout
         layout = QFormLayout()
+
+        # Set vertical spacing of rows.
+        # This will make the spacing remain consistent.
+        layout.setVerticalSpacing(30)
+
         # Add widgets to the layout'
         layout.addRow("Name:", QLineEdit())
         layout.addRow("Job:", QLineEdit())
         emailLabel = QLabel("Email:")
         layout.addRow(emailLabel, QLineEdit())
+
         # Set the layout on the application's window
         self.setLayout(layout)
 
