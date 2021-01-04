2a8e3dfb7fe43e21390354148304eb95245a0e30
Ricky Mitchell <ramitch91@gmail.com>
2021-01-04 00:18:30 -0600

    added bar chart to excel

---
+++ RealPython/openpyxl/xl_barchart.py
@@ -0,0 +1,32 @@
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Reference
+
+workbook = Workbook()
+sheet = workbook.active
+
+# sample data
+rows = [
+    ["Product", "Online", "Store"],
+    [1, 30, 45],
+    [2, 40, 30],
+    [3, 40, 25],
+    [4, 50, 50],
+    [5, 30, 25],
+    [6, 25, 35],
+    [7, 20, 40],
+]
+
+for row in rows:
+    sheet.append(row)
+
+chart = BarChart()
+data = Reference(worksheet=sheet,
+                 min_row=1,
+                 max_row=8,
+                 min_col=2,
+                 max_col=3)
+
+chart.add_data(data, titles_from_data=True)
+sheet.add_chart(chart, "E2")
+
+workbook.save(filename="xl_barchart.xlsx")
