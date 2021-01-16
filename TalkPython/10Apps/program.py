0a8c6f58a486839994f73a9acd6bc7947964635a
ricky mitchell <ramitch91@gmail.com>
2021-01-16 17:43:14 -0600

    changes to Hello App and guess_the_number

---
+++ TalkPython/10Apps/program.py
@@ -0,0 +1,8 @@
+# Header
+print("----------------------------------------")
+print("               Hello App")
+print("----------------------------------------")
+print()
+
+user_name = input("What is your name? ")
+print(f"Nice to meet you {user_name}.")
