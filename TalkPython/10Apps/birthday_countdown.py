b6d0b94d069fb3031c264e69f2e3f6561d5ccd07
ricky mitchell <ramitch91@gmail.com>
2021-01-17 06:01:38 -0600

    add birthday_countdown app

---
+++ TalkPython/10Apps/birthday_countdown.py
@@ -0,0 +1,20 @@
+# Filename: birthday_countdown.py
+
+# imports
+import datetime
+
+
+def main():
+    show_header()
+
+
+def show_header():
+    print()
+    print("----------------------------")
+    print("  Birthday Countdown App")
+    print("----------------------------")
+    print()
+
+
+if __name__ == "__main__":
+    main()
