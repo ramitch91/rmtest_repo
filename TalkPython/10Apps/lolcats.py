b2f8544cfa6bf2e4e983b28e6a46a43d1bf716b9
ricky mitchell <ramitch91@gmail.com>
2021-01-30 18:20:26 -0600

    lol cat factory app

--- TalkPython/10Apps/lolcats.py
+++ TalkPython/10Apps/lolcats.py
@@ -1,13 +1,15 @@
 import os
-import shutil
+import cat_service
+import subprocess
+import platform
 
 
 def main():
     print_header()
     folder = get_or_create_output_folder()
-    print(folder)
-    # download cats
-    # display cats
+    print(f"Found or created {folder}")
+    download_cats(folder)
+    display_cats(folder)
 
 
 def print_header():
@@ -28,6 +30,28 @@ def get_or_create_output_folder():
         os.mkdir(full_path)
 
     return full_path
+
+
+def download_cats(folder):
+    print("Contacting server to download cats...")
+    cat_count = 8
+    for i in range(1, cat_count + 1):
+        name = f"lolcat_{i}"
+        print(f"Downloading cat {name}")
+        cat_service.get_cat(folder, name)
+    print("done")
+
+
+def display_cats(folder):
+    print("Displaying cats in OS window")
+    if platform.system() == "Windows":
+        subprocess.call(["explorer", folder])
+    elif platform.system() == "Linux":
+        subprocess.call(["xdg-open", folder])
+    elif platform.system() == "Darwin":
+        subprocess.call(["open", folder])
+    else:
+        print(f"We don't support your OS {platform.system()}")
 
 
 if __name__ == "__main__":
